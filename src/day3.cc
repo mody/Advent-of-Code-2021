922da9da49b16a0bfbf9c610c2bb547cf63c4d69
Patrik Modesto <patrik.modesto@gmail.com>
2021-12-03 08:48:50 +0100

    Day3/2

--- src/day3.cc
+++ src/day3.cc
@@ -1,21 +1,17 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
 
-int main()
+using Input = std::vector<std::string>;
+using MostLeast = std::pair<std::string, std::string>;
+
+MostLeast most_least(Input const& input)
 {
-    std::vector<std::string> input;
-
-    std::string line;
-    while(std::getline(std::cin, line)) {
-        assert(!line.empty());
-        input.push_back(std::move(line));
-    }
-
-    assert(!input.empty());
-
     std::string most, least;
+    most.resize(input.at(0).size());
+    least.resize(input.at(0).size());
 
     for (size_t i = 0; i < input.at(0).size(); ++i) {
         int zeros = 0, ones = 0;
@@ -26,19 +22,63 @@ int main()
                 ++zeros;
             }
         }
-        if (ones > zeros) {
-            most.append("1");
-            least.append("0");
+        if (ones >= zeros) {
+            most.at(i) = '1';
+            least.at(i) = '0';
         } else {
-            most.append("0");
-            least.append("1");
+            most.at(i) = '0';
+            least.at(i) = '1';
         }
     }
+    return {most, least};
+}
+
+template<typename OP>
+size_t filter_input(Input input, OP&& op)
+{
+    assert(!input.empty());
+
+    for (size_t i = 0; i < input.at(0).size(); ++i) {
+        std::string const& bits = op(most_least(input));
+        const auto c = bits.at(i);
+
+        auto end = std::remove_if(input.begin(), input.end(), [&](std::string const& line) { return line.at(i) != c; });
+        input.erase(end, input.end());
+
+        if (input.size() == 1) {
+            break;
+        }
+    }
+
+    assert(input.size() == 1);
+    return std::stoul(input.at(0), 0, 2);
+}
+
+int main()
+{
+    Input input;
+
+    std::string line;
+    while(std::getline(std::cin, line)) {
+        if (line.empty()) {
+            break;
+        }
+        input.push_back(std::move(line));
+    }
+
+    assert(!input.empty());
+
+    auto const& [most, least] = most_least(input);
 
     auto gamma = stoll(most, 0, 2);
     auto epsilon = stoll(least, 0, 2);
 
     std::cout << "gamma: " << gamma << ", epsilon: " << epsilon << "\n1: " << gamma*epsilon << "\n";
+
+    auto oxygen = filter_input(input, [](MostLeast const& ml) { return ml.first; });
+    auto co2 = filter_input(input, [](MostLeast const& ml) { return ml.second; });
+
+    std::cout << "oxygen: " << oxygen << ", CO2: " << co2 << "\n2: " << oxygen*co2 << "\n";
 
     return 0;
 }
