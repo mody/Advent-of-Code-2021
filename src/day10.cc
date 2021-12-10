5f1381bc17e5f0e3e908f253bf4bd4b5d6b6794e
Patrik Modesto <patrik.modesto@gmail.com>
2021-12-10 09:24:56 +0100

    Day10/2

--- src/day10.cc
+++ src/day10.cc
@@ -1,45 +1,53 @@
 #include <algorithm>
-#include <cassert>
 #include <deque>
 #include <iostream>
 #include <map>
 #include <optional>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
-std::optional<char> check_parenthes(std::string const& input)
+static const std::map<char, char> PARENTHESES = {
+    {'(', ')'},
+    {'<', '>'},
+    {'{', '}'},
+    {'[', ']'},
+};
+
+std::pair<std::optional<char>, std::string> check_parenthes(std::string const& input)
 {
-    static const std::map<char, char> parentheses = {
-        {'(', ')'},
-        {'<', '>'},
-        {'{', '}'},
-        {'[', ']'},
-    };
-
     std::deque<char> expected;
 
     for (auto const& c : input) {
-        auto it = parentheses.find(c);
-        if (it == parentheses.end()) {
-            if (c == expected.back()) {
-                expected.pop_back();
+        auto it = PARENTHESES.find(c);
+        if (it == PARENTHESES.end()) {
+            if (c == expected.front()) {
+                expected.pop_front();
                 continue;
             }
-            return c;
+            return {c, {}};
         }
-        expected.push_back(it->second);
+        expected.push_front(it->second);
     }
-    return {};
+    std::string missing;
+    for (auto const& c : expected) {
+        missing += c;
+    }
+    return std::make_pair(std::nullopt, missing);
 }
 
 int main()
 {
     std::map<char, size_t> counts;
+    std::vector<std::string> missing;
 
     std::string line;
     while (std::getline(std::cin, line)) {
-        auto x = check_parenthes(line);
-        if (!x) { continue; }
+        auto const& [x, _missing] = check_parenthes(line);
+        if (!x) {
+            missing.push_back(_missing);
+            continue;
+        }
         counts.insert({*x, 0}).first->second++;
     }
 
@@ -54,6 +62,24 @@ int main()
     }
 
     std::cout << "1: " << result1 << "\n";
+
+    std::vector<uint64_t> counts2;
+    for (auto const& line : missing) {
+        uint64_t result2 = 0;
+        for (auto const& c : line) {
+            result2 *= 5;
+            switch (c) {
+            case ')': result2 += 1; break;
+            case ']': result2 += 2; break;
+            case '}': result2 += 3; break;
+            case '>': result2 += 4; break;
+            }
+        }
+        counts2.push_back(result2);
+    }
+    std::sort(counts2.begin(), counts2.end());
+
+    std::cout << "2: " << counts2.at(counts2.size() / 2) << "\n";
 
     return 0;
 }
