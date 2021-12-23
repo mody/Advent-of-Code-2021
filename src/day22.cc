a17261dd9025157fe794073b610a34412dd7892a
Patrik Modesto <pmodesto@idirect.net>
2021-12-23 18:59:01 +0100

    Day22/2

--- src/day22.cc
+++ src/day22.cc
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <boost/container_hash/hash.hpp>
 
 #include <cassert>
+#include <cmath>
 #include <iostream>
 #include <limits>
 #include <regex>
@@ -8,99 +10,125 @@
 #include <unordered_set>
 #include <vector>
 
-struct Point {
-    int64_t x = 0, y = 0, z = 0;
 
-    bool operator==(Point const& o) const noexcept { return x == o.x && y == o.y && z == o.z; }
+struct Step {
+    bool on_off = false;
+    int64_t from_x = 0;
+    int64_t to_x = 0;
+    int64_t from_y = 0;
+    int64_t to_y = 0;
+    int64_t from_z = 0;
+    int64_t to_z = 0;
 };
-
-Point operator+(Point lhs, Point const& rhs) noexcept {
-    lhs.x += rhs.x;
-    lhs.y += rhs.y;
-    lhs.z += rhs.z;
-    return lhs;
-}
 
 namespace std {
 
-template <>
-struct hash<Point>
+std::ostream& operator<<(std::ostream& os, Step const& s)
 {
-    size_t operator()(Point const& p) const noexcept
-    {
-        size_t seed = 0;
-        boost::hash_combine(seed, p.x);
-        boost::hash_combine(seed, p.y);
-        boost::hash_combine(seed, p.z);
-        return seed;
-    }
-};
+    os << (s.on_off ? "on" : "off") << " x=" << s.from_x << ".." << s.to_x << ",y=" << s.from_y << ".." << s.to_y
+       << ",z=" << s.from_z << ".." << s.to_z << "\n";
+    return os;
+}
 
 }  // namespace std
 
 
-struct World
+using Steps = std::vector<Step>;
+
+uint64_t process(Steps const& steps)
 {
-    World(int64_t l)
-        : limit {l}
-    { }
+    std::vector<int64_t> X, Y, Z;
 
-    void insert(
-        bool on_off, int64_t _from_x, int64_t _to_x, int64_t _from_y, int64_t _to_y, int64_t _from_z, int64_t _to_z)
-    {
-        if (limit) {
-            int64_t from_x = std::max(-limit, _from_x);
-            int64_t to_x = std::min(limit, _to_x);
-            int64_t from_y = std::max(-limit, _from_y);
-            int64_t to_y = std::min(limit, _to_y);
-            int64_t from_z = std::max(-limit, _from_z);
-            int64_t to_z = std::min(limit, _to_z);
-            insert_impl(on_off, from_x, to_x, from_y, to_y, from_z, to_z);
-        } else {
-            insert_impl(on_off, _from_x, _to_x, _from_y, _to_y, _from_z, _to_z);
-        }
+    auto coord_to_index = [](std::vector<int64_t> const& v, int64_t coord) -> size_t {
+        auto it = std::lower_bound(v.begin(), v.end(), coord);
+        return std::distance(v.begin(), it);
+    };
+
+    for (auto const& s : steps) {
+        X.push_back(s.from_x);
+        X.push_back(s.to_x);
+        Y.push_back(s.from_y);
+        Y.push_back(s.to_y);
+        Z.push_back(s.from_z);
+        Z.push_back(s.to_z);
     }
+    std::sort(X.begin(), X.end());
+    std::sort(Y.begin(), Y.end());
+    std::sort(Z.begin(), Z.end());
 
-    size_t size() const
-    {
-        return world.size();
-    }
+    const auto N = X.size();
+    std::vector<std::vector<std::vector<bool>>> world(
+        N, std::vector<std::vector<bool>>(N, std::vector<bool>(N, false)));
 
-protected:
-    void insert_impl(
-        bool on_off, int64_t from_x, int64_t to_x, int64_t from_y, int64_t to_y, int64_t from_z, int64_t to_z)
-    {
-        for (int64_t x = from_x; x <= to_x; ++x) {
-            for (int64_t y = from_y; y <= to_y; ++y) {
-                for (int64_t z = from_z; z <= to_z; ++z) {
-                    const size_t h = std::hash<Point>()({x,y,z});
-                    if (on_off) {
-                        world.insert(h);
-                    } else {
-                        world.erase(h);
-                    }
+    for (auto const& s : steps) {
+        const auto idx_x = coord_to_index(X, s.from_x);
+        const auto idx_y = coord_to_index(Y, s.from_y);
+        const auto idx_z = coord_to_index(Z, s.from_z);
+
+        const auto to_x = coord_to_index(X, s.to_x);
+        const auto to_y = coord_to_index(Y, s.to_y);
+        const auto to_z = coord_to_index(Z, s.to_z);
+
+        for (auto x = idx_x; x < to_x; ++x) {
+            for (auto y = idx_y; y < to_y; ++y) {
+                for (auto z = idx_z; z < to_z; ++z) {
+                    world[x][y][z] = s.on_off;
                 }
             }
         }
     }
 
-    int64_t limit = 0;
-    std::unordered_set<size_t> world;
-};
+    uint64_t count = 0;
+    for (size_t x = 0; x < N; ++x) {
+        for (size_t y = 0; y < N; ++y) {
+            for (size_t z = 0; z < N; ++z) {
+                if (world[x][y][z]) {
+                    count += (X.at(x + 1) - X.at(x)) * (Y.at(y + 1) - Y.at(y)) * (Z.at(z + 1) - Z.at(z));
+                }
+                // count += (world[x][y][z]) * (X[x + 1] - X[x]) * (Y[y + 1] - Y[y]) * (Z[z + 1] - Z[z]);
+            }
+        }
+    }
+
+    return count;
+}
+
+
+void part1(Steps steps) {
+    auto limitter50 = [](int64_t val) -> int64_t {
+        if (val < -50) {
+            return -50;
+        }
+        if (val > 51) {
+            return 51;
+        }
+        return val;
+    };
+
+    for (auto& s : steps) {
+        s.from_x = limitter50(s.from_x);
+        s.from_y = limitter50(s.from_y);
+        s.from_z = limitter50(s.from_z);
+
+        s.to_x = limitter50(s.to_x);
+        s.to_y = limitter50(s.to_y);
+        s.to_z = limitter50(s.to_z);
+    }
+
+    auto r = process(steps);
+    std::cout << "1: " << r << "\n";
+}
+
+
+void part2(Steps steps) {
+    auto r = process(steps);
+    std::cout << "2: " << r << "\n";
+}
 
 int main(int argc, char* argv[]) {
-    // on x=10..12,y=10..12,z=10..12
     static const std::regex RX{"^(on|off) x=([0-9-]+)\\.\\.([0-9-]+),y=([0-9-]+)\\.\\.([0-9-]+),z=([0-9-]+)\\.\\.([0-9-]+)$"};
 
-    World world50(50);
-    World world(0);
-
-    int64_t min_x = std::numeric_limits<int64_t>::max();
-    int64_t min_y = std::numeric_limits<int64_t>::max();
-    int64_t min_z = std::numeric_limits<int64_t>::max();
-    int64_t max_x = std::numeric_limits<int64_t>::min();
-    int64_t max_y = std::numeric_limits<int64_t>::min();
-    int64_t max_z = std::numeric_limits<int64_t>::min();
+    Steps steps;
 
     std::string line;
     while(std::getline(std::cin, line)) {
@@ -115,32 +143,20 @@ int main(int argc, char* argv[]) {
         }
         assert(m.size() == 8);
 
-        bool on_off = m.str(1) == "on";
-        int64_t from_x = std::stoll(m.str(2));
-        int64_t to_x = std::stoll(m.str(3));
-        int64_t from_y = std::stoll(m.str(4));
-        int64_t to_y = std::stoll(m.str(5));
-        int64_t from_z = std::stoll(m.str(6));
-        int64_t to_z = std::stoll(m.str(7));
+        steps.emplace_back();
+        auto& step = steps.back();
 
-        min_x = std::min(min_x, from_x);
-        min_y = std::min(min_y, from_y);
-        min_z = std::min(min_z, from_z);
-
-        max_x = std::max(max_x, to_x);
-        max_y = std::max(max_y, to_y);
-        max_z = std::max(max_z, to_z);
-
-        world50.insert(on_off, from_x, to_x, from_y, to_y, from_z, to_z);
-        // world.insert(on_off, from_x, to_x, from_y, to_y, from_z, to_z);
+        step.on_off = m.str(1) == "on";
+        step.from_x = std::stoll(m.str(2));
+        step.to_x = std::stoll(m.str(3)) + 1;
+        step.from_y = std::stoll(m.str(4));
+        step.to_y = std::stoll(m.str(5)) + 1;
+        step.from_z = std::stoll(m.str(6));
+        step.to_z = std::stoll(m.str(7)) + 1;
     }
 
-    std::cout << min_x << ".." << max_x << " = " << (max_x - min_x + 1) << "\n";
-    std::cout << min_y << ".." << max_y << " = " << (max_y - min_y + 1) << "\n";
-    std::cout << min_z << ".." << max_z << " = " << (max_z - min_z + 1) << "\n";
-
-    std::cout << "1 Result=" << world50.size() << "\n";
-    std::cout << "2 Result=" << world.size() << "\n";
+    part1(steps);
+    part2(steps);
 
     return 0;
 }
